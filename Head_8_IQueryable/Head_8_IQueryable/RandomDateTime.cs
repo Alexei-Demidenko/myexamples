79f3d04e9fad49b4eaa58ca2c94436171c1fb32c
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-06-30 09:10:57 +0300

    Переделанный

---
+++ Head_8_IQueryable/Head_8_IQueryable/RandomDateTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Head_8_IQueryable
+{
+    internal class RandomDateTime
+    {
+        public DateTime Start { get; set; }
+        public Random gen;
+        public int range;
+        public RandomDateTime()
+        {
+            Start = new DateTime(1935, 1, 1);
+            gen = new Random();
+            range = (DateTime.Today - Start).Days;
+        }
+        public DateTime Next()
+        {
+            return Start.AddDays(gen.Next(range)).AddHours(gen.Next(0, 24)).AddMinutes(gen.Next(0, 60)).AddSeconds(gen.Next(0, 60));
+        }
+    }
+}
