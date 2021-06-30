80efa6bb956b7c88695e217ffca33741a440e98e
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-06-30 15:21:04 +0300

    Переделанный

---
+++ Head_11_Dictionary/Head_11_Dictionary/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Head_11_Dictionary
+{
+    internal class Person
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Middle_name { get; set; }
+        public DateTime Date_Birth { get; set; }
+        public string Place_Birth { get; set; }
+        public int PassportID { get; set; }
+        public Person()
+        {
+        }
+        public Person(string Surname, string Name, string Middle_name, DateTime Date_Birth, string Place_Birth, int PassportID)
+        {
+            this.Surname = Surname;
+            this.Name = Name;
+            this.Middle_name = Middle_name;
+            this.Date_Birth = Date_Birth;
+            this.Place_Birth = Place_Birth;
+            this.PassportID = PassportID;
+        }
+        public override bool Equals(Object obj)
+        {
+            Person person = obj as Person;
+            return Surname.CompareTo(person.Surname) == 0
+                   && Name.CompareTo(person.Name) == 0
+                   && Middle_name.CompareTo(person.Middle_name) == 0
+                   && Place_Birth.CompareTo(person.Place_Birth) == 0
+                   && PassportID.CompareTo(person.PassportID) == 0;
+        }
+        public override int GetHashCode()
+        {
+            return Surname.GetHashCode() + Name.GetHashCode() + Middle_name.GetHashCode() + Place_Birth.GetHashCode() + PassportID.GetHashCode();
+        }
+    }
+}
---
+++ Head_11_Dictionary/Head_11_Dictionary/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Head_11_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new("Демиденко", "Алексей", "Владимирович", new DateTime(1991, 12, 31), "Бутор", 0123456);
+            Person person2 = new("Соломоненко", "Анастасия", "Васильевна", new DateTime(1999, 12, 31), "Тирасполь", 789456);
+
+            Dictionary<Person, string> WorkDictionary = new()
+            {
+                { person1, "УВО МВД ПМР" },
+                { person2, "Dex" }
+            };
+
+            while (true)
+            {
+                Person person3 = new();
+                Console.WriteLine("Введите фамилию:");
+                person3.Surname = Console.ReadLine();
+
+                Console.WriteLine("Введите имя:");
+                person3.Name = Console.ReadLine();
+
+                Console.WriteLine("Введите отчество:");
+                person3.Middle_name = Console.ReadLine();
+
+                Console.WriteLine("Введите место рождения:");
+                person3.Place_Birth = Console.ReadLine();
+
+            PassID:
+                Console.WriteLine("Введите номер паспорта:");
+
+                bool isNum = int.TryParse(Console.ReadLine(), out int num);
+                if (isNum)
+                {
+                    person3.PassportID = num;
+                }
+                else
+                {
+                    Console.WriteLine("ОШИБКА! Введенный номер паспорта не соответсвует формату!\n");
+                    goto PassID;
+                }
+
+                // ищем сотрудника  
+                if (WorkDictionary.TryGetValue(person3, out string employee))
+                {
+                    Console.WriteLine($"Сотрудник найден, его место работы - {employee}\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("Сотрудник не найден!\n\n");
+                }
+            }
+        }
+    }
+}
