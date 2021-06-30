e49d5f5a6655c6f51e11d6aa83ded6e55f529433
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-06-30 13:26:19 +0300

    Переделанный

---
+++ Head_10_GetHashCode_Equals/Head_10_GetHashCode_Equals/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Head_10_GetHashCode_Equals
+{
+    internal class Person
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Middle_name { get; set; }
+        public DateTime Date_Birth { get; set; }
+        public Other_info Other_Info { get; set; }
+        public Person(string Surname, string Name, string Middle_name, DateTime Date_Birth, Other_info other_Info)
+        {
+            this.Surname = Surname;
+            this.Name = Name;
+            this.Middle_name = Middle_name;
+            this.Date_Birth = Date_Birth;
+            Other_Info = other_Info;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is Person))
+            {
+                return false;
+            }
+
+            Person p = (Person)obj;
+
+            return Surname == p.Surname && Name == p.Name && Middle_name == p.Middle_name
+                     && Date_Birth == p.Date_Birth && Other_Info.Place_Birth == p.Other_Info.Place_Birth
+                     && Other_Info.PassportID == p.Other_Info.PassportID;
+        }
+        public override int GetHashCode()
+        {
+            return Other_Info.GetHashCode() + Surname.GetHashCode() + Name.GetHashCode() + Middle_name.GetHashCode() + Date_Birth.GetHashCode();
+        }
+    }
+
+    internal struct Other_info
+    {
+        public string Place_Birth { get; set; }
+        public int PassportID { get; set; }
+        public Other_info(string Place_Birth, int PassportID)
+        {
+            this.Place_Birth = Place_Birth;
+            this.PassportID = PassportID;
+        }
+        public override int GetHashCode()
+        {
+            return PassportID.GetHashCode() + Place_Birth.GetHashCode();
+        }
+    }
+}
---
+++ Head_10_GetHashCode_Equals/Head_10_GetHashCode_Equals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Head_10_GetHashCode_Equals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Other_info ot_info = new("Бутор", 0123456);
+            Other_info ot_info1 = new("Тирасполь", 789456);
+            Person person1 = new("Демиденко", "Алексей", "Владимирович", new DateTime(1991, 12, 31), ot_info);
+            Person person2 = new("Демиденко", "Алексей", "Владимирович", new DateTime(1991, 12, 31), ot_info);
+            Person person3 = new("Соломоненко", "Анастасия", "Васильевна", new DateTime(1991, 12, 31), ot_info1);
+
+            Console.WriteLine($"Метод Equals: person1 == person2 = {person1.Equals(person2)}");
+            Console.WriteLine($"Метод Equals: person1 == person3 = {person1.Equals(person3)}");
+            Console.WriteLine($"Метод GetHashCode: person1 = {person1.GetHashCode()}");
+            Console.WriteLine($"Метод GetHashCode: person2 = {person2.GetHashCode()}");
+            Console.WriteLine($"Метод GetHashCode: ot_info = {ot_info.GetHashCode()}");
+            Console.WriteLine($"Метод GetHashCode: ot_info1 = {ot_info1.GetHashCode()}");
+        }
+    }
+}
