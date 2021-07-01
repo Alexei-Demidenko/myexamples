6e7b8fdff6fb7d1001c5fac821a307bbe2d6e01d
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-07-01 13:15:48 +0300

    Пул потоков.

---
+++ Head_21_ThreadPool/Head_21_ThreadPool/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Head_21_ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.GetMaxThreads(out int nWorkerThreads, out int nCompletionThreads);
+            Console.WriteLine($"Максимальное количество потоков: {nWorkerThreads}" +
+                 $"\nПотоков ввода-вывода доступно: {nCompletionThreads}");
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadPool.QueueUserWorkItem(JobForAThread);
+            }
+            Thread.Sleep(3000);
+            Console.ReadLine();
+        }
+
+        static void JobForAThread(object state)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"цикл {i}, выполнение внутри потока из пула {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
