c9c909a613e4cca27f2630791f3a37a77bc7fb0c
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-07-01 08:30:45 +0300

    События. Интерфейс INotifyPropertyChanged.

---
+++ Head_14_INotifyPropertyChanged/Head_14_INotifyPropertyChanged/Car_modelClass.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Head_14_INotifyPropertyChanged
+{
+    internal class Car_modelClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        string car_model;
+        public string Car_model
+        {
+            get { return car_model; }
+            set
+            {
+                car_model = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Car_model)));
+            }
+        }
+    }
+}
---
+++ Head_14_INotifyPropertyChanged/Head_14_INotifyPropertyChanged/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Head_14_INotifyPropertyChanged
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Car_modelClass sample = new();
+            sample.PropertyChanged += new PropertyChangedEventHandler(Sample_PropertyChanged);
+            while (true)
+            {
+                Console.WriteLine($"Введите марку авто: ");
+                sample.Car_model = Console.ReadLine();
+            }
+        }
+
+        private static void Sample_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Car_modelClass sample = (Car_modelClass)sender;
+            Console.WriteLine($"Значение свойства {e.PropertyName} было изменено! Новое значение: {sample.Car_model}\n");
+        }
+    }
+}
