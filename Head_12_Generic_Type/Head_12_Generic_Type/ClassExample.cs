2358b1b61b1eef9e64356081a5b0db3d115c0f16
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-07-02 13:48:28 +0300

    List<T>

---
+++ Head_12_Generic_Type/Head_12_Generic_Type/ClassExample.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Head_12_Generic_Type
+{
+    internal class ClassExample<T>
+    {
+        private readonly List<T> ListValue = new() { };
+		public void AddValue(T value)
+        {
+            ListValue.Add(value);
+		}
+        public List<T> GetListValue => ListValue;
+    }
+}
---
+++ Head_12_Generic_Type/Head_12_Generic_Type/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Head_12_Generic_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаем список помещений
+            var RoomName = new ClassExample<string>();
+
+            //заполняем значениями
+            RoomName.AddValue("Коридор");
+            RoomName.AddValue("Кабинет");
+            RoomName.AddValue("Кухня");
+
+            ////Создаем список площадь помещений
+            var RoomArea = new ClassExample<int>();
+            RoomArea.AddValue(20);
+            RoomArea.AddValue(35);
+            RoomArea.AddValue(15);
+
+            Console.WriteLine("Название помещений (список RoomName, тип string):");
+            foreach (var item in RoomName.GetListValue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nПлощать помещений (список RoomArea, тип int):");
+            foreach (var item in RoomArea.GetListValue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
