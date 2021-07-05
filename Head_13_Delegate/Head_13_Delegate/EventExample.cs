d565e9ced203546f55e8a916bfe9d8abc6a05de8
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-07-05 11:15:30 +0300

    Делегаты. Лямбда выражения. События.

---
+++ Head_13_Delegate/Head_13_Delegate/EventExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Head_13_Delegate
+{
+    class EventExample
+    {
+        //Свойство OnChange содержит список всех callback-методов подписчиков 
+        public event Action OnChange = delegate { };
+
+        public void Mothod()
+        {
+            Console.WriteLine("\nВызов метода  Mothod из класса EventExample");
+            //Вызов OnChange
+            OnChange();
+        }
+    }
+}
---
+++ Head_13_Delegate/Head_13_Delegate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Head_13_Delegate
+{
+    public delegate void MathDelegate(double value1, double value2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathDelegate mathDelegate = Add;
+            mathDelegate += Subtract;// MathDelegate указывает на два метода Add и Subtract
+            Console.WriteLine("\nВызов делегата  mathDelegate:");
+            mathDelegate(5, 2);// вызываются оба метода из mathDelegate
+            mathDelegate -= Subtract; // удаляем метод Subtract из mathDelegate      
+            MathDelegate mathDelegate2 = Subtract;
+            MathDelegate mathDelegate3 = mathDelegate + mathDelegate2;// объединяем делегаты mathDelegate + mathDelegate2
+            Console.WriteLine("\nВызов делегата  mathDelegate3 при помоши Invoke:");
+            mathDelegate3.Invoke(7, 2);
+
+            MathDelegate mathDelegateMulti = (x, y) =>
+            {
+                Console.WriteLine($"\nДелегат mathDelegateMulti: {x} * {y} = {x * y}");
+            };
+            mathDelegateMulti(7, 2);
+            
+            EventExample eventExample = new();// Инициализируем объект класса EventExample    
+            eventExample.OnChange += () =>// подписываем вывод на событие OnChange 
+            {
+                // Устанавливаем красный цвет символов
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Произошло действие!Был вызован метод  Mothod.");
+                // Сбрасываем настройки цвета
+                Console.ResetColor();
+            };
+            eventExample.Mothod();// генерируем событие  
+        }
+        
+        public static void Add(double value1, double value2)
+        {
+            Console.WriteLine($"Метода Add: {value1 + value2}");
+        }
+        public static void Subtract(double value1, double value2)
+        {
+            Console.WriteLine($"Метода Subtract: {value1 - value2}");
+        }        
+    }
+}
