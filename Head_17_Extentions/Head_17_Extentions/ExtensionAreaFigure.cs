b74cf54aa7b3813ce91ec45293cd1172f799608e
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-07-05 15:01:58 +0300

    Расширения.

---
+++ Head_17_Extentions/Head_17_Extentions/ExtensionAreaFigure.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Head_17_Extentions
+{
+    public static class ExtensionAreaFigure
+    {
+        // Площадь прямоугольного треугольника
+        public static double RightTriangle(this AreaFigure areaFigure, double CathetusA, double CathetusC) =>
+            0.5 * (CathetusA * CathetusC);
+    }
+}
---
+++ Head_17_Extentions/Head_17_Extentions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Head_17_Extentions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AreaFigure areaFigure = new();
+            int value = 5;
+
+            Console.WriteLine($"Площадь квадрата = {areaFigure.Square(value)}");
+            Console.WriteLine($"Площадь круга = {areaFigure.Circle(value)}");
+            Console.WriteLine($"Площадь прямоугольного треугольника = {areaFigure.RightTriangle(value, value)}");
+        }
+    }
+}
