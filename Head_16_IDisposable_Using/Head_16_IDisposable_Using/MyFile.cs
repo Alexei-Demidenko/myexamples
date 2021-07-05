30c7300e0559bcac1cf9c6b06602c88e0563dfa6
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-07-05 13:53:49 +0300

    IDisposable. Ключевое слово using.

---
+++ Head_16_IDisposable_Using/Head_16_IDisposable_Using/MyFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Head_16_IDisposable_Using
+{
+    class MyFile : IDisposable
+    {
+        // 1000 байт неуправляемых ресурсов
+        public IntPtr ptr = Marshal.AllocHGlobal(1000);
+        public virtual void Dispose()
+        {
+            Console.WriteLine("Метод Disposed. Освобождаем неуправляемый ресурс ptr");
+            // освобождаем ресурс
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+}
---
+++ Head_16_IDisposable_Using/Head_16_IDisposable_Using/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Head_16_IDisposable_Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			MyFile myFile = null;
+            try
+            {
+                myFile = new MyFile(); // инициализируем
+                Console.WriteLine("Создан объект класса MyFile!");
+            }
+            finally
+            {
+                if (myFile != null)
+				{
+                    myFile.Dispose();
+                }
+            }
+
+            Console.WriteLine("\nСоздаем объект класса MyFile в конструкций using");
+            using MyFile file = new();                        
+        }
+    }
+}
