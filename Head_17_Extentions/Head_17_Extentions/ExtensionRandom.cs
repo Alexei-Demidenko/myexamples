2289ca872af81b0111ea0b0b8327b7aa9cfc7c8f
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-07-22 16:11:50 +0300

    Переделанный

--- Head_17_Extentions/Head_17_Extentions/ExtensionRandom.cs
+++ Head_17_Extentions/Head_17_Extentions/ExtensionRandom.cs
@@ -4,7 +4,7 @@ namespace Head_17_Extentions
 {
     internal static class ExtensionRandom
     {
-        internal static void PrintRandom(this Random skydiving, int RandomValue)
+        internal static void PrintRandom(this Random randomdom, int RandomValue)
         {
             Console.WriteLine($"\nМетод расширения PrintRandom класса Random:");
             Console.WriteLine($"Сгенерировано случайное число: {RandomValue}.");
