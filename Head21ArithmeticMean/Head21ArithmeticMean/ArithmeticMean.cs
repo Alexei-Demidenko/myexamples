21f3fbd3dfb2abe7c4923117b93103983049dd2d
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-07-26 15:48:59 +0300

    Среднее арифметическое.

---
+++ Head21ArithmeticMean/Head21ArithmeticMean/ArithmeticMean.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Head21ArithmeticMean
+{
+    internal class ArithmeticMean
+    {       
+        internal static Stopwatch Time = new();
+        internal static void Arithmetic(int ArrayCount)
+        {
+            int[] Array = new int[ArrayCount];
+            int ArraySum = 0;
+            Random rnd = new();
+            Time.Start();
+            for (int i = 0; i < ArrayCount; i++)
+            {
+                Array[i] = rnd.Next(1, 100);
+                ArraySum += Array[i];
+            }
+            ArraySum /= ArrayCount;
+            Time.Stop();
+            Print(ArraySum, Time.Elapsed);
+        }
+        internal static void ParallelForEachArithmeticMean(int ArrayCount)
+        {
+            int[] Array = new int[ArrayCount];
+            int BatchSize = 100;
+            int ArraySum = 0;
+            Random rnd = new();
+            Time.Start();
+            Parallel.ForEach(Partitioner.Create(0, Array.Length, BatchSize), range =>
+            {
+                for (int i = range.Item1; i < range.Item2; i++)
+                {
+                    Array[i] = rnd.Next(1, 100);
+                    ArraySum += Array[i];
+                }
+            });
+            ArraySum /= ArrayCount;
+            Time.Stop();
+            Print(ArraySum, Time.Elapsed);
+        }
+        internal static void ParallelForArithmeticMean(int ArrayCount)
+        {
+            int[] Array = new int[ArrayCount];
+            int ArraySum = 0;
+            Random rnd = new();
+            Time.Start();
+            Parallel.For(0, Array.Length, n => { Array[n] = rnd.Next(1, 100); ArraySum += Array[n]; });
+            ArraySum /= ArrayCount;
+            Time.Stop();
+            Print(ArraySum, Time.Elapsed);
+        }
+        private static void Print(int ArithmeticMean, TimeSpan Time)
+        {
+            Console.WriteLine($"Среднея арифметическое: {ArithmeticMean}");
+            Console.WriteLine($"Cреднее арифметическое сгенерирована и подсчитана за {Time} миллисек.");
+        }
+    }
+}
---
+++ Head21ArithmeticMean/Head21ArithmeticMean/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Head21ArithmeticMean
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Start();
+        }
+        private static void Start()
+        {
+            int[] ArrayCount = { 10000, 100000 };          
+            Console.WriteLine("\tПараллельное вычисления средней арифметическое(Parallel.ForEach):");
+            foreach (int item in ArrayCount)
+            {
+                ArithmeticMean.ParallelForEachArithmeticMean(item);
+            }
+            Console.WriteLine("\n\tПараллельное вычисления средней арифметическое(Parallel.For):");
+            foreach (int item in ArrayCount)
+            {
+                ArithmeticMean.ParallelForArithmeticMean(item);
+            }
+            Console.WriteLine("\n\tСредней арифметическое без параллельных вычислений:");
+            foreach (int item in ArrayCount)
+            {
+                ArithmeticMean.Arithmetic(item);
+            }
+        }
+    }
+}
