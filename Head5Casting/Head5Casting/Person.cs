86aa43b641758ae9968a621d108c1c036a368176
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-08-16 13:06:42 +0300

    Casting. Класс Person (FirstName, LastName) и строка "Иванов Иван".

---
+++ Head5Casting/Head5Casting/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Head5Casting
+{
+    internal class Person
+    {
+        internal string FirstName { get; set; }
+        internal string LastName { get; set; }
+
+        public static explicit operator Person(string surnameName)
+        {
+            try
+            {
+                var arrayName = surnameName.Split(new char[] { ' ', ',', '.', ':' });
+                var person = new Person { FirstName = arrayName[0], LastName = arrayName[1] };
+                return person;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            throw new Exception($"Не удалось преобразовать строку: {surnameName} в Person.");
+        }
+        public override int GetHashCode() => $"{FirstName} {LastName}".GetHashCode();
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj.GetType() != GetType()) return false;
+            var person = (Person)obj;
+            return person != null && (FirstName == person.FirstName && LastName == person.LastName);
+        }
+        internal void Print(string exemplarName)
+        {
+            Console.WriteLine($"{exemplarName}: {FirstName} {LastName}");
+        }
+    }
+}
---
+++ Head5Casting/Head5Casting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Head5Casting
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            var person1 = new Person { FirstName = "Алексей", LastName = "Демиденко" };
+            person1.Print("person1");
+            var person2 = (Person)"Алексей Демиденко";
+            person2.Print("person2");
+            Console.Write("Сравниваем экземпляр Person и текст при помощи оператора == : ");
+            Console.WriteLine(person1 == (Person)"Алексей Демиденко");
+            Console.Write("Сравниваем экземпляр Person и текст при помощи Equals() : ");
+            Console.WriteLine(person1.Equals((Person)"Алексей Демиденко"));
+        }
+    }
+}
