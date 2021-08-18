830389c18a39ab693cc2d19a6ba1e242d3efe83a
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-08-18 10:59:45 +0300

    + проверка содержится ли добавляемый элемент в списке.

--- Head_12_Generic_Type/Head_12_Generic_Type/ClassExample.cs
+++ Head_12_Generic_Type/Head_12_Generic_Type/ClassExample.cs
@@ -5,33 +5,40 @@ namespace Head_12_Generic_Type
 {
     internal class ClassExample<T>: IComparer<T> where T : Room
     {
-        public List<T> RommCollection { get; } = new() { };
-        public void Add(T ValueRoom)
+        public List<T> RommCollection { get; } = new();
+        public void Add(T valueRoom)
         {
-            if (!string.IsNullOrEmpty(ValueRoom.RoomName))
+            if (!string.IsNullOrEmpty(valueRoom.RoomName))
             {
-                RommCollection.Add(ValueRoom);
-                Console.WriteLine($"Комната:-{ValueRoom.RoomName}- добавлена в списке.");
+                if (RommCollection.Contains(valueRoom))
+                {
+                    throw new Exception($"Комната:-{valueRoom.RoomName}- уже содержится в списке.");
+                }
+                else
+                {
+                    RommCollection.Add(valueRoom);
+                    Console.WriteLine($"Комната:-{valueRoom.RoomName}- добавлена в списке.");
+                }
             }
         }
-        internal static void Print(T ValueRoom)
+        internal static void Print(T valueRoom)
         {
-            if (ValueRoom.Free)
+            if (valueRoom.Free)
             {
-                Console.WriteLine($"Комната: -{ValueRoom.RoomName}-, с плозщадью {ValueRoom.RoomArea} кв. м. - свободна");
+                Console.WriteLine($"Комната: -{valueRoom.RoomName}-, с плозщадью {valueRoom.RoomArea} кв. м. - свободна");
             }
             else
             {
-                Console.WriteLine($"Комната: -{ValueRoom.RoomName}-, с плозщадью {ValueRoom.RoomArea} кв. м. - занята");
+                Console.WriteLine($"Комната: -{valueRoom.RoomName}-, с плозщадью {valueRoom.RoomArea} кв. м. - занята");
             }
         }
-        public int Compare(T ValueRoom1, T ValueRoom2)
+        public int Compare(T valueRoom1, T valueRoom2)
         {
-            if (ValueRoom1.RoomArea > ValueRoom2.RoomArea)
+            if (valueRoom2 != null && valueRoom1 != null && valueRoom1.RoomArea > valueRoom2.RoomArea)
             {
                 return 1;
             }
-            else if (ValueRoom1.RoomArea < ValueRoom2.RoomArea)
+            else if (valueRoom2 != null && valueRoom1 != null && valueRoom1.RoomArea < valueRoom2.RoomArea)
             {
                 return -1;
             }
--- Head_12_Generic_Type/Head_12_Generic_Type/Program.cs
+++ Head_12_Generic_Type/Head_12_Generic_Type/Program.cs
@@ -2,29 +2,37 @@
 
 namespace Head_12_Generic_Type
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main()
         {
             Room room = new() { RoomName = "Номер 101", RoomArea = 95, Free = true };
             Room room2 = new() { RoomName = "Номер 222", RoomArea = 35, Free = false };
             Room room3 = new() { RoomName = "Номер 333", RoomArea = 40, Free = true };
             Room room4 = new() { RoomName = "Номер 404", RoomArea = 75, Free = false };
             ClassExample<Room> classExample = new();
-            classExample.Add(room);
-            classExample.Add(room2);
-            classExample.Add(room3);
-            classExample.Add(room4);
-            Console.WriteLine($"\n\tСодержимое RommCollection:");
-            foreach (Room ValueRoom in classExample.RommCollection)
+            try
             {
-                ClassExample<Room>.Print(ValueRoom);
+                classExample.Add(room);
+                classExample.Add(room2);
+                classExample.Add(room3);
+                classExample.Add(room4);
+                classExample.Add(room4);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine($"\n\tСодержимое RommCollection:");
+            foreach (var valueRoom in classExample.RommCollection)
+            {
+                ClassExample<Room>.Print(valueRoom);
             }
             Console.WriteLine($"\n\tСодержимое RommCollection после сортировке по полю RoomArea:");
             classExample.RommCollection.Sort(classExample);
-            foreach (Room ValueRoom in classExample.RommCollection)
+            foreach (var valueRoom in classExample.RommCollection)
             {
-                ClassExample<Room>.Print(ValueRoom);
+                ClassExample<Room>.Print(valueRoom);
             }
         }
     }
