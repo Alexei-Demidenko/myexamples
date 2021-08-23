6804dda68b0cb90ed19ce03af1d29a67178956eb
Alexei-Demidenko <alexeidemid6699@gmail.com>
2021-08-23 15:17:39 +0300

    переделал

---
+++ Head_14.1_Events/Head_14.1_Events/Jumper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Head_14._1_Events
+{
+    internal class Jumper
+    {
+        public delegate void SkydiverHandler();
+        public event SkydiverHandler Notify;
+        internal void Height(int height)
+        {
+            if (height is 3900 or 1000 or 100)
+            {
+                Notify?.Invoke();
+            }
+            else
+            {
+                Console.WriteLine($"Ты находишся на высоте :{height} м.");
+            }
+        }
+    }
+}
--- Head_14.1_Events/Head_14.1_Events/Program.cs
+++ Head_14.1_Events/Head_14.1_Events/Program.cs
@@ -1,12 +1,54 @@
 ﻿
+using System;
+using System.Threading;
+
 namespace Head_14._1_Events
 {
     internal class Program
     {
+        private static readonly Jumper _handler = new();
         internal static void Main(string[] args)
         {
-            Parachutist classParachutist = new();
-            classParachutist.Jump();
+            _handler.Notify += Message1;
+            for (var i = 4000; i > 0; i -= 100)
+            {
+                switch (i)
+                {
+                    case 3900:
+                        _handler.Height(i);
+                        _handler.Notify -= Message1;
+                        _handler.Notify += Message2;
+                        break;
+                    case 1000:
+                        _handler.Height(i);
+                        _handler.Notify -= Message2;
+                        _handler.Notify += Message3;
+                        break;
+                    case 100:
+                        _handler.Height(i);
+                        _handler.Notify -= Message3;
+                        break;
+                    default:
+                        _handler.Height(i);
+                        Thread.Sleep(50);
+                        break;
+                }
+            }
         }
+        internal static void Message1()
+        {
+            Console.WriteLine("\nПоздравляем! Ты спрыгнул с парашютом.\n");
+            Thread.Sleep(50);
+        }
+        internal static void Message2()
+        {
+            Console.WriteLine("\nНе забудьте открыть парашют!\n");
+            Thread.Sleep(50);
+        }
+        public static void Message3()
+        {
+            Console.WriteLine("\nПоздравляем! Ты приземлился!\nНадеемся всё прошло удачно.");
+        }
+
     }
 }
